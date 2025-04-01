e0c746b6424d6eaa4da59e7da5527e8a1c94b8d6
Ida <idax3451@stud.kea.dk>
2025-04-01 23:09:19 +0200

    adding-forms

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/*
+document.querySelector("#headCircle").addEventListener("mouseover", headHighlight);
+document.querySelector("#headCircle").addEventListener("mouseout", headUnHighlight);
+document.querySelector("#headCircle").addEventListener("click", headInfo);
+
+
+function headHighlight() {
+    console.log("headHighlight");
+    document.querySelector("#headCircle").style.fill = #ffffff;
+}
+
+function headHighUnlight() {
+    console.log("headHighUnlight");
+    document.querySelector("#headCircle").style.fill = #000000;
+}
+
+function headInfo() {
+    console.log("headInfo");
+    document.querySelector(".info-text > h2").textContent = "Hovedet !";
+document.querySelector(".info-text > article > p").textContent = "lorem ipsum"
+}
+*/
+
+/*
+document.querySelector("btn1").addEventListener("click", investigate);
+document.querySelector("btn2").addEventListener("click", investigate2);
+document.querySelector("btn3").addEventListener("click", investigate3);
+
+function investigate() {
+console.log("btn1");
+document.querySelector("btn1").style.backgroundColor = "#f00";
+}
+
+function investigate() {
+console.log("btn2");
+document.querySelector("btn2").style.backgroundColor = "#f00";
+}
+
+function investigate() {
+console.log("btn3");
+document.querySelector("btn3").style.backgroundColor = "#f00";
+}
+
+Alternative solution
+function investigate (){
+console.log(this);
+this.style.backgroundColor = "f00";
+}
+/*
+
+/*
+function headHighlight .....
+
+function headHighlight (){
+    console.log("headHighlight");
+    this.style.fill = "#f00";
+}
+    /*
